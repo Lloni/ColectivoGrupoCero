57f4ee8e1abf10505c1025799bed9c84f4f17d0a
Lloni <jonathan.pacheco@gmail.com>
2021-05-22 09:21:00 -0400

    corrección en validaciones

--- funcion-validar-cuenta.js
+++ funcion-validar-cuenta.js
@@ -1,48 +1,72 @@
-var validaNombre = function() {
+var validaNombre = function(){
     var nom = document.getElementById("idnombre").value;
-    if (nom.length > 3) {
+    if(nom.length>0){
         return true;
-    } else {
+    }else{
         return false;
     }
 }
 
-var validarEmail = function() {
-    var contra = document.getElementById("idcorreo").value;
-    re = /^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
-    if (!re.exec(contra)) {
+var validarCorreo = function(){
+    var correo = document.getElementById("idcorreo").value;
+    if(correo.length>0){
+        return true;
+    }else{
         return false;
-    } else {
-        return true
-    };
+    }
 }
 
-
-var validarContraseña = function() {
+var validarContraseña = function(){
     var contra = document.getElementById("idcontraseña").value;
-    if (contra.length > 6) {
+    if(contra.length>0){
         return true;
-    } else {
+    }else{
         return false;
     }
 }
 
 
-var validarFunciones = function() {
-    if (validaNombre() == true) {} else {
-        envio = "El campo nombre debe ser ingresado";
+var validarFunciones =function(){
+    if(validaNombre()==true){
+    }else{
+        envio="El campo nombre debe ser ingresado";
         return envio;
     }
-    if (validarEmail() == true) {} else {
-        envio = "Debe ingresar un correo (Ejemplo: Correo123@gmail.com)"
+    if(validarCorreo()==true){
+    }else{
+        envio="Debe ingresar un correo (Ejemplo: Correo123@gmail.com)"
         return envio;
     }
-    if (validarContraseña() == true) {} else {
-        envio = "El campo contraseña debe contener como mínimo 7 caracteres";
+    if(validarContraseña()==true){
+    }else{
+        envio="El campo contraseña debe contener como mínimo 7 caracteres";
         return envio;
     }
-    if (validaNombre() == true && validarEmail() == true && validarContraseña() == true) {
-        envio = "Ingreso exitoso, Bienvenido!";
+    if(validaNombre()==true&&validarCorreo()==true&&validarContraseña()==true){
+        envio="CUENTA CREADA CORRECTAMENTE";
+        window.location.href = "publicacion.html";
         return envio;
     }
 }
+
+
+// var validarFunciones =function(){
+//     if !(validaNombre()==true){
+//         envio="El campo nombre debe ser ingresado";
+//         return false;
+        
+//     } else if !(validarCorreo()==true){
+//         envio="Debe ingresar un correo (Ejemplo: Correo123@gmail.com)"
+//         return false;
+//     }
+//     else if !(validarContraseña()==true){
+//         envio="El campo contraseña debe contener como mínimo 7 caracteres";
+//         return false;
+//     } else if (validaNombre()==true&&validarCorreo()==true&&validarContraseña()==true){
+//         envio="CUENTA CREADA CORRECTAMENTE";
+//         return false;
+//     } else {
+//         window.location.href = "publicacion.html";
+//     }
+
+// }
