8404bf7ae25d36708875107d17fa1e0a6859d591
ManuelDiaz15 <Mana.diaz@duocuc.cl>
2021-05-21 22:52:15 -0400

    Funcion-validar-cuenta

---
+++ funcion-validar-cuenta.js
@@ -0,0 +1,49 @@
+var validaNombre = function(){
+    var nom = document.getElementById("idnombre").value;
+    if(nom.length>0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+var validarCorreo = function(){
+    var correo = document.getElementById("idcorreo").value;
+    if(correo.length>0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+var validarContraseña = function(){
+    var contra = document.getElementById("idcontraseña").value;
+    if(contra.length>0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
+var validarFunciones =function(){
+    if(validaNombre()==true){
+    }else{
+        envio="El campo nombre debe ser ingresado";
+        return envio;
+    }
+    if(validarCorreo()==true){
+    }else{
+        envio="Debe ingresar un correo (Ejemplo: Correo123@gmail.com)"
+        return envio;
+    }
+    if(validarContraseña()==true){
+    }else{
+        envio="El campo contraseña debe contener como mínimo 7 caracteres";
+        return envio;
+    }
+    if(validaNombre()==true&&validarCorreo()==true&&validarContraseña()==true){
+        envio="FORMULARIO DE CONSULTA SE HA ENVIADO CON ÉXITO";
+        return envio;
+    }
+}
