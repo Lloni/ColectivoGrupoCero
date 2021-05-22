70a6b4dd77204e6cd5e0e60399a237fb242d961c
Malcom Pozo <malcompozo@gmail.com>
2021-05-21 23:20:56 -0400

    correccion de validaciones

--- funcion-validar-cuenta.js
+++ funcion-validar-cuenta.js
@@ -1,49 +1,46 @@
-var validaNombre = function(){
+var validaNombre = function() {
     var nom = document.getElementById("idnombre").value;
-    if(nom.length>0){
+    if (nom.length > 0) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
 
-var validarCorreo = function(){
+var validarCorreo = function() {
     var correo = document.getElementById("idcorreo").value;
-    if(correo.length>0){
+    if (correo.length > 0) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
 
-var validarContraseña = function(){
+var validarContraseña = function() {
     var contra = document.getElementById("idcontraseña").value;
-    if(contra.length>0){
+    if (contra.length > 6) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
 
 
-var validarFunciones =function(){
-    if(validaNombre()==true){
-    }else{
-        envio="El campo nombre debe ser ingresado";
+var validarFunciones = function() {
+    if (validaNombre() == true) {} else {
+        envio = "El campo nombre debe ser ingresado";
         return envio;
     }
-    if(validarCorreo()==true){
-    }else{
-        envio="Debe ingresar un correo (Ejemplo: Correo123@gmail.com)"
+    if (validarCorreo() == true) {} else {
+        envio = "Debe ingresar un correo (Ejemplo: Correo123@gmail.com)"
         return envio;
     }
-    if(validarContraseña()==true){
-    }else{
-        envio="El campo contraseña debe contener como mínimo 7 caracteres";
+    if (validarContraseña() == true) {} else {
+        envio = "El campo contraseña debe contener como mínimo 7 caracteres";
         return envio;
     }
-    if(validaNombre()==true&&validarCorreo()==true&&validarContraseña()==true){
-        envio="FORMULARIO DE CONSULTA SE HA ENVIADO CON ÉXITO";
+    if (validaNombre() == true && validarCorreo() == true && validarContraseña() == true) {
+        envio = "Ingreso exitoso, Bienvenido!";
         return envio;
     }
 }
