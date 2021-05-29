9d05072243fe1812d681863acedef042d44a590e
Malcom Pozo <malcompozo@gmail.com>
2021-05-29 19:28:26 -0400

    arreglo de template tags css y js

--- app/static/app/js/funcion-validar-cuenta.js
+++ app/static/app/js/funcion-validar-cuenta.js
@@ -41,29 +41,7 @@ var validarFunciones = function() {
     }
     if (validaNombre() == true && validarCorreo() == true && validarContraseña() == true) {
         envio = "CUENTA CREADA CORRECTAMENTE";
-        window.location.href = "publicacion.html";
+        window.location.href = "{% url 'publicacion' %}";
         return envio;
     }
 }
-
-
-// var validarFunciones =function(){
-//     if !(validaNombre()==true){
-//         envio="El campo nombre debe ser ingresado";
-//         return false;
-
-//     } else if !(validarCorreo()==true){
-//         envio="Debe ingresar un correo (Ejemplo: Correo123@gmail.com)"
-//         return false;
-//     }
-//     else if !(validarContraseña()==true){
-//         envio="El campo contraseña debe contener como mínimo 7 caracteres";
-//         return false;
-//     } else if (validaNombre()==true&&validarCorreo()==true&&validarContraseña()==true){
-//         envio="CUENTA CREADA CORRECTAMENTE";
-//         return false;
-//     } else {
-//         window.location.href = "publicacion.html";
-//     }
-
-// }
