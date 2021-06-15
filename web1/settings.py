16d18bae701643b3bb153f438ab52157d598acd2
Malcom Pozo <malcompozo@gmail.com>
2021-06-14 21:23:02 -0400

    conectando sqlite nuevamente

--- web1/settings.py
+++ web1/settings.py
@@ -74,13 +74,10 @@ WSGI_APPLICATION = 'web1.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.oracle',
-        'NAME': '127.0.0.1:1521/xepdb1',
-        'USER': 'django',
-        'PASSWORD': 'django',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
